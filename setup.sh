6fbf1d573bfebaff394cb40bb071e1a5408bf224
norbertbai <norbert.majubu@benevolent.ai>
2020-01-16 15:42:32 +0000

    convert setup script into requirements.txt

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python3
-pip3 install pandas
-pip3 install matplotlib
-pip3 install xlrd
-pip3 install jupyter
-jupyter notebook course.ipynb
